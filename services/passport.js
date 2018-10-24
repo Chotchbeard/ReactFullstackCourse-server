e151e194b6a47739ce23cfdf9caf530cbaf503aa
Jacob Senteney <jwsenteney@gmail.com>
2018-10-24 18:51:52 -0400

    setting up cookies, and the ability to retrieve a user record based on cookie contents

--- services/passport.js
+++ services/passport.js
@@ -5,8 +5,25 @@ const keys = require("../config/keys");
 
 const User = mongoose.model("users");
 
+/*
+ * This gets executed when the done function
+ * gets called from the GoogleStrategy callback.
+ * It creates an identifying token for the user
+ * (just their mongo id in our case) and puts it
+ * in a cookie.
+ */
 passport.serializeUser((user, done) => {
   done(null, user.id);
+});
+
+/*
+ * This gets the user's record from the db based
+ * on the token in their cookie.
+ */
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
 });
 
 passport.use(
